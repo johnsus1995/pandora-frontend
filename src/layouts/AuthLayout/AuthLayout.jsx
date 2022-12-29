48d1c401192b42bb632ecc32d4f8ddbe2e070ec4
Jaison John <jaisonjohny26@gmail.com>
2022-12-29 21:33:45 +0530

    style fixes to Authlayout navbar

--- src/layouts/AuthLayout/AuthLayout.jsx
+++ src/layouts/AuthLayout/AuthLayout.jsx
@@ -6,12 +6,16 @@ const AuthLayout = () => {
   return (
     <div className={`${styles.AuthLayout} container-fluid`}>
       <nav className="navbar">
-        <h2>NEXCC</h2>
-        <ul className="right">
-          <li>Option1</li>
-          <li>Option2</li>
-          <li>Logout</li>
-        </ul>
+        <div className="left">
+          <h3>NEXCC</h3>
+        </div>
+        <div className="right">
+          <ul>
+            <li>Option1</li>
+            <li>Option2</li>
+            <li>Logout</li>
+          </ul>
+        </div>
       </nav>
       <Outlet />
     </div>
