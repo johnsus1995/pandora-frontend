e6f2a67678d8cda9b8180bdebc75b7c658ed0a7d
Jaison John <jaisonjohny26@gmail.com>
2022-12-29 21:15:17 +0530

    router initial setup

--- src/layouts/AuthLayout/AuthLayout.jsx
+++ src/layouts/AuthLayout/AuthLayout.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import styles from "./AuthLayout.module.scss"
+import React from "react";
+import { Outlet } from "react-router-dom";
+import styles from "./AuthLayout.module.scss";
 
 const AuthLayout = () => {
   return (
-    <div className={`${styles.AuthLayout}`}>AuthLayout</div>
-  )
-}
+    <div className={`${styles.AuthLayout} container-fluid`}>
+      <nav className="navbar">
+        <h2>NEXCC</h2>
+        <ul className="right">
+          <li>Option1</li>
+          <li>Option2</li>
+          <li>Logout</li>
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -4,6 +4,12 @@ const Register = props => {
 
   return (
     <div className={`${styles.Register }`}>
+       The first line of Lorem Ipsum, "Lorem ipsum dolor sit
+      amet..", comes from a line in section 1.10.32. The standard chunk of Lorem
+      Ipsum used since the 1500s is reproduced below for those interested.
+      Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by
+      Cicero are also reproduced in their exact original form, accompanied by
+      English versions from the 1914 translation by H. Rackham
     </div>
   );
 };
